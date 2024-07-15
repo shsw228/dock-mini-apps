fcbd2dd22039bbcfcd6ee2a2d4ef9508d177b669
Tkng <13506491+shsw228@users.noreply.github.com>
2024-07-15 22:03:09 +0900

    add ping feature

--- check_alive/__init__.py
+++ check_alive/__init__.py
@@ -1,5 +1,7 @@
 import lvgl as lv
-import display_driver
+import logging
+from .ping_utils import ping
+import uasyncio as asyncio
 
 # The name of the application to be displayed on the menu
 NAME = "Check Alive"
@@ -7,12 +9,20 @@ NAME = "Check Alive"
 # A file path or data (bytes type) of the logo image for this app.
 # If not specified, the default icon will be applied.
 ICON = ""
-
+logger = logging.getLogger('check_alive')
 # LVGL widgets
 scr = None
 label1 = None
 label2 = None
 label3 = None
+resultLabel1 = None
+resultLabel2:Label = None
+resultLabel3:Label = None
+
+# value
+result1 = False
+result2 = False
+result3 = False
 
 # Setting params
 ip1 = None
@@ -24,7 +34,6 @@ frequency = DEFAULT_FREQUENCY
 
 app_mgr = None
 ping_now = False
-
 
 def get_user_setting():
     global ip1, ip2, ip3, frequency
@@ -41,18 +50,55 @@ async def on_stop():
 async def on_start():
     # User triggered to enter this app, all features should be activated
     print('on_start')
-    global scr, label1, label2, label3
-    
+    global scr, label1, label2, label3, result1, result2, result3
+
+    # スタイルを定義し、パディングを設定
+    style = lv.style_t()
+    style.init()
+    style.set_pad_all(10)  # 全てのパディングを10に設定
     
     get_user_setting()
     scr = lv.obj()
-    lv.screen_active().set_flex_flow(lv.FLEX_FLOW.COLUMN)
-    #lv.screen_active().set_flex_align(lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.START, lv.FLEX_ALIGN.CENTER)
-    label1 = lv.label(scr)
-    label2 = lv.label(scr)
-    label3 = lv.label(scr)
+    container = lv.obj(scr)
+    container.set_size(lv.pct(100), lv.pct(50))
+    container.add_style(style,0)
+    
+    container2 = lv.obj(scr)
+    container2.set_size(lv.pct(100), lv.pct(50))
+    resultLabel1 = lv.label(container2)
+    resultLabel1.set_text(str(result1))
+    resultLabel2 = lv.label(container2)
+    resultLabel2.set_text(str(result2))
+    resultLabel3 = lv.label(container2)
+    resultLabel3.set_text(str(result3))
+    
+    container.set_flex_flow(lv.FLEX_FLOW.COLUMN)
+    container2.set_flex_flow(lv.FLEX_FLOW.COLUMN)
+    container2.align_to(container, lv.ALIGN.OUT_BOTTOM_MID, 0, 0)
+    label1 = lv.label(container)
+    label2 = lv.label(container)
+    label3 = lv.label(container)
     update_label()
+    
+    
+    hosts = [] # 初期化
+    hosts = [ip1, ip2, ip3]  # Pingするホストのリスト
+    tasks = [ping(host, count=4) for host in hosts]
+    results = await asyncio.gather(*tasks)
+    for result, host in zip(results, hosts):
+        print(f"{host} results: {result}")
+        if host == ip1:
+            print(f"This is ip1")
+        if host == ip2:
+            print(f"This is ip2")
+        if host == ip3:
+            print(f"This is ip3")
+        
+    update_ping_status()
+
     lv.scr_load(scr)
+        
+    
 
 async def on_boot(apm):
     global app_mgr
@@ -71,6 +117,15 @@ def update_label():
         label2.set_text(ip2)
     if label3:
         label3.set_text(ip3)
+
+def update_ping_status():
+    global resultLabel1, resultLabel2, resultLabel3
+    if resultLabel1:
+        resultLabel1.set_text(str(result1))
+    if resultLabel2:
+        resultLabel2.set_text(str(result2))
+    if resultLabel3:
+        resultLabel3.set_text(str(result3))
 
 def get_settings_json():
     return {
@@ -105,3 +160,6 @@ def get_settings_json():
             "attributes": {"maxLength": 15, "placeholder": "Enter device ip(192.168.0.0)"}
         }]
     }
+
+
+
